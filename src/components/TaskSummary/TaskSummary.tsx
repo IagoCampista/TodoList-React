d8dde5580ec98514e55b58e947dcbf5d8fa4c0b9
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2021-08-22 12:47:55 -0300

    criado o taskSummary

---
+++ src/components/TaskSummary/TaskSummary.tsx
@@ -0,0 +1,24 @@
+import { Container } from "./styles"
+
+export function TaskSummary (){
+
+    return(
+        <Container>
+            <h2>Resumo das Tasks</h2>
+
+            <div className='Card'>
+            <header>
+                <p>Total Tasks</p>
+            </header>
+            <strong>5</strong>
+            </div>
+
+            <div className='Card'>
+            <header>
+                <p>Completed Tasks</p>
+            </header>
+            <strong>35</strong>
+            </div>
+        </Container>
+    )
+}
--- src/components/TaskSummary/styles.ts
+++ src/components/TaskSummary/styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-        
+
+    max-width: 800px;
+    width: 80%;
+
+    align-items: center;
+
 `;
